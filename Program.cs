908cfef6919e4b1bf8f2017ababddf55e72920c2
Diana Avila <missernightangel@gmail.com>
2025-08-30 20:09:12 -0300

    moedas

--- Program.cs
+++ Program.cs
@@ -1,22 +1,23 @@
-﻿using dio_net_explorando.Models;
+﻿using System.Globalization;
+using dio_net_explorando.Models;
 
-Pessoa pessoa1 = new Pessoa();
-pessoa1.NomeAluno = "Vampiro";
-pessoa1.SobrenomeAluno = "Drácula";
-pessoa1.IdadeAluno = 100;
-pessoa1.Apresentar();
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.NomeAluno = "Vampiro";
+// pessoa1.SobrenomeAluno = "Drácula";
+// pessoa1.IdadeAluno = 100;
+// pessoa1.Apresentar();
 
-Pessoa pessoa2 = new Pessoa("Frankenstein", "Stein", 150);
-Pessoa pessoa3 = new Pessoa("Zé", "Ninguém", 20);
-Pessoa pessoa4 = new Pessoa("Maria", "Silva", 30);
+// Pessoa pessoa2 = new Pessoa("Frankenstein", "Stein", 150);
+// Pessoa pessoa3 = new Pessoa("Zé", "Ninguém", 20);
+// Pessoa pessoa4 = new Pessoa("Maria", "Silva", 30);
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.NomeCurso = "Inglês";
-cursoDeIngles.AdicionarAluno(pessoa1);
-cursoDeIngles.AdicionarAluno(pessoa2);
-cursoDeIngles.AdicionarAluno(pessoa3);
-cursoDeIngles.AdicionarAluno(pessoa4);
-cursoDeIngles.ListarAlunos();
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.NomeCurso = "Inglês";
+// cursoDeIngles.AdicionarAluno(pessoa1);
+// cursoDeIngles.AdicionarAluno(pessoa2);
+// cursoDeIngles.AdicionarAluno(pessoa3);
+// cursoDeIngles.AdicionarAluno(pessoa4);
+// cursoDeIngles.ListarAlunos();
 
 // Curso cursoDeProgramacao = new Curso("Programação C#");
 // cursoDeProgramacao.AdicionarAluno(pessoa2);
@@ -24,3 +25,13 @@ cursoDeIngles.ListarAlunos();
 
 // Console.WriteLine($"Total de alunos no curso de Inglês: {cursoDeIngles.ContarAlunos()}");
 // Console.WriteLine($"Total de alunos no curso de Programação C#: {cursoDeProgramacao.ContarAlunos()}");
+
+// // Formatação em moeda corrente do sistema
+// decimal valor = 1215.5M;
+// Console.WriteLine($"{valor:C}"); // C de currency (moeda)
+// Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+
+// // Alterando a localidade para mudar o formato da moeda
+// System.Globalization.CultureInfo.DefaultThreadCurrentCulture = new System.Globalization.CultureInfo("en-US");
+// Console.WriteLine($"{valor:C}"); // C de currency (moeda)
+
